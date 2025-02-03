d145a125cb5d623f9946c3f5a96ad2b2e5c019af
sorin04 <sorin.pirgari@lyceeastier.com>
2025-02-03 09:47:43 +0100

    commit4

--- serveur/routes/users.js
+++ serveur/routes/users.js
@@ -1,10 +1,7 @@
-// /routes/users.js
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { login } = require('../controllers/connexionController');
+const { login } = require("../controllers/connexionController");
 
-
-// Route POST pour la connexion
-router.post('/login', login);
+router.post("/login", login);
 
 module.exports = router;
